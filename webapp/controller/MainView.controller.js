37da064eb562fdf19deaab42de64871043d79b0c
Gerstenberg Sebastian <s.gerstenberg@sap.com>
2015-10-27 07:42:21 +0000

    gif player

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -5,12 +5,22 @@ sap.ui.define([
 
 	return Controller.extend("com.sap.innojam.pic.controller.MainView", {
 		onInit : function(evt) {
+			var vendors=jQuery.sap.getModulePath("com.sap.innojam.pic.vendors", "");
 			var model = new sap.ui.model.json.JSONModel({
-				title:"some title"
+				title:"some title",
+				images:jQuery.sap.getModulePath("com.sap.innojam.pic.resources", "/images"),
+				animations:jQuery.sap.getModulePath("com.sap.innojam.pic.resources", "/animations"),
+				vendors:vendors
 			});
 			this.getView().setModel(model);
-			
-			
+			jQuery.sap.registerModulePath("abc", "abc"); 
+			jQuery.getScript(vendors+"/libgif.js", function() {
+			    jQuery.getScript(vendors+"/rubbable.js", function() {
+					var sup2 = new RubbableGif({ gif: document.getElementById('example2') } );
+					sup2.load();
+			    });
+				
+			});
 		},
 		onPress : function(event) {
 			sap.m.MessageToast.show("first toast");
